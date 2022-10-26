b111f88676b679d91b916be859d02caa6bdf7a71
axis <axisotherwise@gmail.com>
2022-10-26 23:58:37 +0900

    Update 회원가입 시 사용자 찾기와 패스워드 해시화 로직 추가

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,12 +3,24 @@ import { Service } from "typedi";
 import { IUserService } from "../types/_.exporter";
 import { CreateUserDto } from "../dtos/_.exporter";
 import { UserDao } from "../daos/_.exporter";
+import { generateHashPassword, compareHashPassword } from "../utils/_.exporter";
 
 @Service()
 export class UserService implements IUserService{
     constructor(private readonly userDao: UserDao) {};
 
-    createUser({ email, password }: CreateUserDto) {
-        return this.userDao.createUser({ email, password });
+    async createUser({ email, password }: CreateUserDto) {
+        const find = await this.userDao.findUserByEmail(email);
+
+        console.log(find);
+
+        const hashPassword = await generateHashPassword(password);
+
+        const input = {
+            email,
+            password: hashPassword,
+        } as CreateUserDto;
+
+        return this.userDao.createUser(input);
     }
 }
